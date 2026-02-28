8a0903a339aba9aee8da0c877ef5c88d1a559149
ronfrancis-reyes <rronfrancis@gmail.com>
2026-02-28 17:42:12 +0800

    finished registration validation (subject to change)

--- validations/registration.js
+++ validations/registration.js
@@ -17,10 +17,67 @@ function checkFields(){
 }
 
 function checkInputs(){
+    const checkEmail = email.value.trim();
+    const checkContact = contact.value.trim();
+    const checkpassword = password.value.trim();
+    const checkRetype = retype.value.trim();
+    const passStrength = passwordStrengthCheck(checkpassword);
 
+
+    if(/^[0-9]+@ms\.bulsu\.edu\.ph$/.test(checkEmail)){
+        if(/^09[0-9]{9}$/.test(checkContact)){
+            if(passStrength < 3){
+                alert("Password weak, create another password!");
+            }
+            else if(passStrength == 3 || passStrength == 4){
+                let proceed = confirm("Password strength is medium. Do you want proceed?");
+                if(proceed){
+                    if(checkpassword === checkRetype){
+                        alert("Account Created!");
+                    }
+                    else{
+                        alert("Password doesn't match!");
+                        retype.focus();
+                    }
+                }
+                else{
+                    password.focus();
+                }
+            }
+            else{
+                if(checkpassword === checkRetype){
+                    alert("Account Created!");
+                }
+                else{
+                    alert("Password doesn't match!");
+                    retype.focus();
+                }
+            }
+        }
+        else{
+            alert("Please use valid contact number!");
+            contact.focus();
+        }
+    }
+    else{
+        alert("Please use distributed BulSU MS Account!");
+        email.focus();
+    }
 }
 
-signupbtn.addEventListener("click", checkInputs)
+function passwordStrengthCheck(password){
+    let strength = 0;
+
+    if(password.length >= 8) strength++;
+    if(/[A-Z]/.test(password)) strength++;
+    if(/[a-z]/.test(password)) strength++;
+    if(/[0-9]/.test(password)) strength++;
+    if(password.includes("@") || password.includes(".") || password.includes("_") ) strength++;
+    
+    return strength;
+}
+
+signupbtn.addEventListener("click", checkInputs);
 Fullname.addEventListener("input", checkFields);
 email.addEventListener("input", checkFields);
 contact.addEventListener("input", checkFields);
