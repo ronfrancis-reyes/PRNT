1934077bd754e4c329f7e9620b54a1c7e5c3fcd6
YanceCabotaje <yancenathanc@gmail.com>
2026-03-16 10:36:46 +0800

    UI: Finalized registration

---
+++ PRNT/pages/about/index.php
@@ -0,0 +1,172 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link rel="preconnect" href="https://fonts.googleapis.com" />
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+		<link
+			href="https://fonts.googleapis.com/css2?family=Geist:wght@100..900&display=swap"
+			rel="stylesheet"
+		/>
+		<link
+			href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css"
+			rel="stylesheet"
+			integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB"
+			crossorigin="anonymous"
+		/>
+		<!--Tab Logo-->
+		<link rel="icon" href="assets/img/PRNT_logo.png" type="image/png" />
+		<!--Logo Library -->
+		<link
+			rel="stylesheet"
+			href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+		/>
+		<link rel="stylesheet" href="/PRNT/components/Navbar/Navbar.css" />
+		<link rel="stylesheet" href="/PRNT/components/Footer/Footer.css" />
+		<link rel="stylesheet" href="styles.css" />
+		<link rel="stylesheet" href="../../global/global.css" />
+		<title>PRNT</title>
+	</head>
+	<body>
+		<!-- heading/navigation bar -->
+		<custom-nav></custom-nav>
+
+		<!-- start of body-->
+		<main>
+			<section class="page page-about">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-7">
+							<small><span class="dot"></span>ABOUT US</small>
+							<h1>Bring your ideas to life with PRNT.</h1>
+							<a href="../service/" class="print-now">PRINT NOW ↗</a>
+						</div>
+						<div class="col-lg-5 text-lg-end mt-5 mt-lg-0">
+							<div class="image-box ms-auto">
+								<img
+									src="/PRNT/assets/img/img19.jpg"
+									class="img-fluid rounded"
+									alt="Printer"
+								/>
+								<p>
+									We help you bring your projects to life by providing
+									high-quality printing, custom designs, and practical solutions
+									that fit your needs.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section class="page page-works">
+				<div class="container">
+					<div class="row align-items-center min-vh-100 py-5">
+						<div class="col-lg-6">
+							<small class="text-white mb-4"
+								><span class="dot bg-white"></span>WHAT WE DO?</small
+							>
+							<img
+								src="/PRNT/assets/img/img20.jpg"
+								class="img-fluid rounded mb-4"
+								alt="Process"
+							/>
+							<p class="works-caption">
+								Every project is crafted with sharp details, bold colors, and a
+								clean finish.
+							</p>
+						</div>
+						<div class="col-lg-6 ps-lg-5">
+							<p class="works-text">
+								PRNT delivers high-quality printing for students, schools, and
+								local businesses.
+							</p>
+							<p class="works-text">
+								We turn your bold ideas into vibrant, professional prints that
+								make an impact.
+							</p>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section class="page page-vision">
+				<div class="container py-5">
+					<div
+						class="row min-vh-100 align-items-center justify-content-center text-center"
+					>
+						<div class="col-12">
+							<small class="justify-content-center"
+								><span class="dot"></span>OUR VISION</small
+							>
+							<h2 class="vision-title mx-auto">
+								Friendly print shop designed to make printing easy and
+								convenient for both students and clients.
+							</h2>
+							<div class="row g-4 mt-5">
+								<div class="col-md-4">
+									<img
+										src="/PRNT/assets/img/img8.jpg"
+										class="vision-img rounded"
+										alt="Vision 1"
+									/>
+								</div>
+								<div class="col-md-4">
+									<img
+										src="/PRNT/assets/img/img10.jpg"
+										class="vision-img rounded"
+										alt="Vision 2"
+									/>
+								</div>
+								<div class="col-md-4">
+									<img
+										src="/PRNT/assets/img/img3.jpg"
+										class="vision-img rounded"
+										alt="Vision 3"
+									/>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section class="page page-mission-wrapper">
+				<div class="container">
+					<div class="row min-vh-100  justify-content-center">
+						<div class="col-lg-11">
+							<div class="mission-card">
+								<small class="justify-content-center"
+									><span class="dot"></span>OUR MISSION</small
+								>
+								<h2 class="mission-text mx-auto">
+									We harness the power of quality printing and creative design
+									to help students and local businesses succeed.
+									<br /><br />
+									Through our reliable and affordable print solutions, we
+									inspire, engage, and deliver results, ensuring every project
+									stands out and meets your needs.
+								</h2>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</main>
+		<!--footer-->
+		<custom-footer></custom-footer>
+
+		<script type="module" src="script.js"></script>
+		<script
+			src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+			integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+			crossorigin="anonymous"
+		></script>
+		<script
+			src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.min.js"
+			integrity="sha384-G/EV+4j2dNv+tEPo3++6LCgdCROaejBqfUeNjuKAiuXbjrxilcCdDz6ZAVfHWe1Y"
+			crossorigin="anonymous"
+		></script>
+	</body>
+</html>
---
+++ PRNT/pages/login/script.js
@@ -0,0 +1,42 @@
+const email = document.getElementById("floatingInput");
+const password = document.getElementById("floatingPassword");
+const submitButt = document.getElementById("submitButton");
+
+const testEmail = "2024100823@ms.bulsu.edu.ph";
+const testPassword = "bulsuStudent123";
+
+function checkFields() {
+	if (email.value.trim() === "" || password.value.trim() === "") {
+		submitButt.disabled = true;
+	} else {
+		submitButt.disabled = false;
+	}
+}
+
+function checkAccount() {
+	if (
+		email.value.trim() === testEmail &&
+		password.value.trim() === testPassword
+	) {
+		alert("Login Successful!");
+	} else {
+		alert("Account does not exist!");
+	}
+}
+
+submitButt.addEventListener("click", checkAccount);
+email.addEventListener("input", checkFields);
+password.addEventListener("input", checkFields);
+
+function showPass() {
+	const passwordInput = document.getElementById("floatingPassword");
+	const toggle = document.getElementById("showPasswordToggle");
+
+	toggle.addEventListener("change", function () {
+		if (this.checked) {
+			passwordInput.type = "text";
+		} else {
+			passwordInput.type = "password";
+		}
+	});
+}
