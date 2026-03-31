9db788659288c51eee8c536c4e9314bc60eaa446
Chunmr <canaredavidgeeno@gmail.com>
2026-03-31 15:13:17 +0800

    unfinished user dashboard api

--- api/dashboard.php
+++ api/dashboard.php
@@ -3,6 +3,15 @@ include "config.php";
 if (isset($_POST['action'])) {
     if ($_POST['action'] == 'get') {
         $id = $_SESSION['account_id'];
-        
+        $sql = $conn->prepare("SELECT * FROM orders WHERE account_id = ?");
+        $sql->bind_param('i', $id);
+        if ($sql->execute()) {
+
+        } else {
+            echo json_encode([
+                'status' => 'error',
+                'message' => 'query failed'
+            ]);
+        }
     }
 }
