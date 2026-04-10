446fa374ed64576ffa385e5d94a721d6cd8a5fa4
Chunmr <canaredavidgeeno@gmail.com>
2026-04-10 10:21:32 +0800

    fixed: responsiveness of client dashboard

--- pages/client/service-avail/index.php
+++ pages/client/service-avail/index.php
@@ -357,6 +357,8 @@ if (!isset($_SESSION['user'])) {
     <script src="https://code.jquery.com/jquery-4.0.0.js"
         integrity="sha256-9fsHeVnKBvqh3FB2HYu7g2xseAZ5MlN6Kz/qnkASV8U=" crossorigin="anonymous"></script>
     <script src="script.js"></script>
+	<script src="../../../global/global.js"></script>
+
 </body>
 
 </html>
